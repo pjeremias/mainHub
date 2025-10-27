d3bec70c4961a89206242e316fb03363b7ec4045
pjeremias <penasjd@gmail.com>
2025-10-27 00:01:35 -0300

    feat: use MQTTManager and DHTManager

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,25 @@
-// src/main.cpp
 #include <Arduino.h>
 #include "WiFiManager.h"
-#include "nvs_flash.h"
+#include "MQTTManager.h"
+#include "DHTManager.h"
 
 WiFiManager wifiManager;
+MQTTManager mqttManager;
+DHTManager sensorManager(mqttManager);
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting ESP32 Hub Firmware");
 
-  nvs_flash_init();
-
   // Initialize WiFi
   wifiManager.begin();
-  
-  // If we reach here, WiFi is connected or configuration is done
-  Serial.println("WiFi setup complete. Proceeding to main loop.");
+  mqttManager.begin();
+  sensorManager.begin();
+
+  Serial.println("Setup complete. Proceeding to main loop.");
 }
 
 void loop() {
-  // Main loop for sensors/actuators will go here later
-  delay(1000);  // Placeholder
+  mqttManager.loop();
+  sensorManager.loop();
 }
