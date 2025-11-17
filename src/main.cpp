7d2de69bcd9cdcc965b3449224cb4e195fd119fa
pjeremias <penasjd@gmail.com>
2025-11-17 17:59:23 -0300

    feat: add code to test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include <Arduino.h>
 #include "WiFiManager.h"
 #include "MQTTManager.h"
-#include "SystemController.h"
+#include "HubSystem.h"
 #include "DHTSensor.h"
 #include "CoolingOutput.h"
-#include <ArduinoJson.h> // delete
+#include "MQTTObserver.h"
 
 WiFiManager wifiManager;
 MQTTManager mqttManager;
-SystemController systemController(mqttManager);
+HubSystem hub;
+DHTSensor tempSensor(4);
+CoolingOutput fridge(24);
+MQTTObserver mqttObserver(mqttManager, hub);
 
 void setup() {
   Serial.begin(115200);
@@ -16,22 +19,18 @@ void setup() {
 
   wifiManager.begin();
   mqttManager.begin();
-  systemController.begin();
+  mqttObserver.begin();
+
+  hub.setObserver(&mqttObserver);
+
+  hub.addSensor("tempSensor1", &tempSensor);
+  hub.addOutput("fridge", &fridge);
+  hub.linkOutputToSensor("fridge", "tempSensor1");
   
-  JsonDocument doc;
-  doc["pin"] = 27;
-  systemController.addSensor("tempSensor", "dht22", doc.as<JsonObjectConst>());
-  
-  JsonDocument outputDoc;
-  outputDoc["pin"] = 14;
-  systemController.addOutput("fridge", "cooling", outputDoc.as<JsonObjectConst>());
-  
-  systemController.attachOutputToSensor("fridge", "tempSensor");
-    
   Serial.println("Setup complete. Proceeding to main loop.");
 }
 
 void loop() {
   mqttManager.loop();  // Handle MQTT connection
-  systemController.loop();  // Handle reading, publishing, and actuation
+  hub.loop();  // Handle sensor updates and output actuation
 }
